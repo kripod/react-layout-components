600ca6275dcd580667adca089f13544a2ce67652
Kristóf Poduszló <kripod@protonmail.com>
2020-06-28 14:59:33 +0200

    refactor(Stack): disallow row direction

--- src/Stack.tsx
+++ src/Stack.tsx
@@ -5,62 +5,38 @@ import * as React from "react";
 import { useRef } from "react";
 
 import { useLogicalCSSPropertyFallback } from "./useLogicalCSSPropertyFallback";
-import { CSSProperties } from "./utils";
+import {
+	CSSProperties,
+	CSSPropertyAlignItems,
+	CSSPropertyJustifyContent,
+	prefixFlexAlignmentValue,
+} from "./utils";
 
-type CSSPropertyAlignItems = Exclude<
-	CSSProperties["alignItems"],
-	"flex-end" | "flex-start"
->;
-type CSSPropertyJustifyContent = Exclude<
-	CSSProperties["justifyContent"],
-	"flex-end" | "flex-start"
->;
-
-// TODO: Consider deprecating "direction" in favor of the Cluster component
 export type StackProps = {
 	as?: React.ElementType;
 	childWrapper?: React.ElementType;
+	alignInline?: CSSPropertyAlignItems;
+	alignBlock?: CSSPropertyJustifyContent;
 	spacing?: CSSProperties["gap"];
+	reverse?: boolean;
 	children?: React.ReactNode;
-} & (
-	| {
-			direction?: "column" | "column-reverse";
-			alignInline?: CSSPropertyAlignItems;
-			alignBlock?: CSSPropertyJustifyContent;
-	  }
-	| {
-			direction: "row" | "row-reverse";
-			alignInline?: CSSPropertyJustifyContent;
-			alignBlock?: CSSPropertyAlignItems;
-	  }
-);
-
-function prefixAlignmentValue(value: string | undefined): string | undefined {
-	// Transform "end"/"start" into the more supported "flex-end"/"flex-start"
-	return value === "end" || value === "start" ? `flex-${value}` : value;
-}
+};
 
 export function Stack({
 	as: Element = "div",
 	childWrapper: ChildWrapper = "div",
-	direction = "column", // Encourage a mobile-first design by default
 	alignInline,
 	alignBlock,
 	spacing,
+	reverse,
 	children,
 }: StackProps): JSX.Element {
 	const lastChildIndex = React.Children.count(children) - 1;
-	const isDirectionColumn = direction[0] === "c";
-	const isDirectionReverse = direction.slice(-1) === "e";
 
 	const elementRef = useRef<HTMLElement>(null);
-	const { blockStart, inlineStart } = useLogicalCSSPropertyFallback(elementRef);
+	const { blockStart } = useLogicalCSSPropertyFallback(elementRef);
 	const spacerClassName = css({
-		[`margin-${
-			isDirectionColumn
-				? blockStart || "block-start"
-				: inlineStart || "inline-start"
-		}`]: spacing,
+		[`margin-${blockStart || "block-start"}`]: spacing,
 	});
 
 	return (
@@ -68,19 +44,15 @@ export function Stack({
 			ref={elementRef}
 			className={css({
 				display: "flex",
-				flexDirection: direction,
-				alignItems: prefixAlignmentValue(
-					isDirectionColumn ? alignInline : alignBlock,
-				),
-				justifyContent: prefixAlignmentValue(
-					isDirectionColumn ? alignBlock : alignInline,
-				),
+				flexDirection: "column",
+				alignItems: prefixFlexAlignmentValue(alignInline),
+				justifyContent: prefixFlexAlignmentValue(alignBlock),
 			})}
 		>
 			{React.Children.map(children, (child, index) => (
 				<ChildWrapper
 					className={
-						index !== (isDirectionReverse ? lastChildIndex : 0)
+						index !== (reverse ? lastChildIndex : 0)
 							? spacerClassName
 							: undefined
 					}
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,19 @@ import * as CSS from "csstype";
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type CSSProperties = CSS.Properties<(string & {}) | number>;
+
+export type CSSPropertyAlignItems = Exclude<
+	CSSProperties["alignItems"],
+	"flex-end" | "flex-start"
+>;
+export type CSSPropertyJustifyContent = Exclude<
+	CSSProperties["justifyContent"],
+	"flex-end" | "flex-start"
+>;
+
+export function prefixFlexAlignmentValue(
+	value: string | undefined,
+): string | undefined {
+	// Transform "end"/"start" into the more supported "flex-end"/"flex-start"
+	return value === "end" || value === "start" ? `flex-${value}` : value;
+}

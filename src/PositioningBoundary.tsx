eb49e824d76136b4a614e3bdae7d7b15307f34b7
Kristóf Poduszló <kripod@protonmail.com>
2020-06-28 08:50:58 +0200

    feat: add PositioningBoundary component

---
+++ src/PositioningBoundary.tsx
@@ -0,0 +1,12 @@
+import { css } from "otion";
+import * as React from "react";
+
+export interface PositioningBoundaryProps {
+	children: React.ReactNode;
+}
+
+export function PositioningBoundary({
+	children,
+}: PositioningBoundaryProps): JSX.Element {
+	return <div className={css({ position: "relative" })}>{children}</div>;
+}

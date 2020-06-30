e5e098ed73e852aeaf4993c02559c49ecfa807ab
Kristóf Poduszló <kripod@protonmail.com>
2020-06-30 22:52:36 +0200

    docs(example): update

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import React from "react";
 
 import { Cluster } from "./Cluster";
 import { Spacer } from "./Spacer";
+import { Stack } from "./Stack";
 
 export default function App(): JSX.Element {
 	return (
-		<Cluster spacing="2em">
-			<div>Logo</div>
-			<Spacer />
-			<div>Menu 1</div>
-			<div>Menu 2</div>
-		</Cluster>
+		<>
+			<Stack spacing={8}>
+				<div>a</div>
+				<div>b</div>
+				<div>c</div>
+			</Stack>
+
+			<Cluster spacing="2em">
+				<div>Logo</div>
+				<Spacer />
+				<div>Menu 1</div>
+				<div>Menu 2</div>
+			</Cluster>
+		</>
 	);
 }

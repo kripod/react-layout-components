b3a4fee016c604e1713992fc7443b73c2e512024
Kristóf Poduszló <kripod@protonmail.com>
2020-07-01 20:51:17 +0200

    docs(example): add usage experiments

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+/* Source of example texts: https://developer.mozilla.org/en-US/docs/Web/CSS/writing-mode#Example */
+
+import { css } from "otion";
 import React from "react";
 
 import { Cluster } from "./Cluster";
@@ -7,18 +10,64 @@ import { Stack } from "./Stack";
 export default function App(): JSX.Element {
 	return (
 		<>
-			<Stack spacing={8}>
-				<div>a</div>
-				<div>b</div>
-				<div>c</div>
-			</Stack>
+			<h1>React Layout Components</h1>
+			<p>
+				Experimentation and examples for cross-browser layout component testing.
+				The project is primarily inspired by{" "}
+				<a href="https://every-layout.dev/">Every Layout</a>.
+			</p>
 
-			<Cluster spacing="2em">
-				<div>Logo</div>
-				<Spacer />
-				<div>Menu 1</div>
-				<div>Menu 2</div>
-			</Cluster>
+			<h2>Cluster</h2>
+
+			<h3>Horizontal (LTR) script</h3>
+			<div className={css({ writingMode: "horizontal-tb" })}>
+				<Cluster spacing="2em">
+					<div>Example text</div>
+					<Spacer />
+					<div>Example text</div>
+					<div>Example text</div>
+				</Cluster>
+			</div>
+
+			<h3>Horizontal (RTL) script</h3>
+			<div className={css({ writingMode: "horizontal-tb", direction: "rtl" })}>
+				<Cluster spacing="2em">
+					<div>מלל ארוך לדוגמא</div>
+					<Spacer />
+					<div>מלל ארוך לדוגמא</div>
+					<div>מלל ארוך לדוגמא</div>
+				</Cluster>
+			</div>
+
+			<h3>Vertical script</h3>
+			<div className={css({ writingMode: "vertical-rl" })}>
+				<Cluster spacing="2em">
+					<div>我家没有电脑。</div>
+					<Spacer />
+					<div>我家没有电脑。</div>
+					<div>我家没有电脑。</div>
+				</Cluster>
+			</div>
+
+			<h2>Stack</h2>
+
+			<h3>Horizontal (LTR) script</h3>
+			<div className={css({ writingMode: "horizontal-tb" })}>
+				<Stack alignInline="start" spacing="1em">
+					<div>Example text</div>
+					<div>Example text</div>
+					<div>Example text</div>
+				</Stack>
+			</div>
+
+			<h3>Horizontal (RTL) script</h3>
+			<div className={css({ writingMode: "horizontal-tb", direction: "rtl" })}>
+				<Stack alignInline="start" spacing="1em">
+					<div>מלל ארוך לדוגמא</div>
+					<div>מלל ארוך לדוגמא</div>
+					<div>מלל ארוך לדוגמא</div>
+				</Stack>
+			</div>
 		</>
 	);
 }

4ceac11c5b1f6a6bd0cc348d4ba00019507e3108
Kristóf Poduszló <kripod@protonmail.com>
2020-07-17 23:57:05 +0200

    refactor(touchTarget): rule declaration order

--- src/mixins.ts
+++ src/mixins.ts
@@ -14,14 +14,14 @@ export const positioningBoundary = css({ position: "relative" });
 export const touchTarget = `${positioningBoundary} ${css({
 	userSelect: "none",
 	"::after": {
-		position: "absolute",
 		content: "''",
+		position: "absolute",
+		top: "50%",
+		left: "50%",
+		transform: "translate(-50%,-50%)",
 		minWidth: 44,
 		minHeight: 44,
 		width: "100%",
 		height: "100%",
-		top: "50%",
-		left: "50%",
-		transform: "translate(-50%,-50%)",
 	},
 })}`;
